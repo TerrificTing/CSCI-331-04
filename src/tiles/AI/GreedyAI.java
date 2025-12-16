d83b7b8af61ff7571aecb496ddc59860a4d9e6ea
TerrificTing <jting200@yahoo.com>
2025-12-15 21:42:05 -0500

    Greedy completed

--- src/tiles/AI/GreedyAI.java
+++ src/tiles/AI/GreedyAI.java
@@ -2,14 +2,42 @@ package tiles.AI;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import src.tiles.model.Direction;
-import src.tiles.model.TilesModel;
+import tiles.model.Direction;
+import tiles.model.TilesModel;
+import java.util.Random;
 
 public class GreedyAI implements AI {
+
     @Override
     public Direction chooseMove(TilesModel model) {
         Direction[] dirs = Direction.values();
-        return dirs[random.nextInt(dirs.length)];
+        Direction move = null;
+        int max = -1;
+        int score = 0;
+
+        for(Direction dir : dirs) {
+            TilesModel current = model.copy();
+
+            current.move(dir);
+
+            if(!current.equals(model)) {
+                System.out.println(current);
+                System.out.println(current.getScore());
+                System.out.println(model);
+                System.out.println(model.getScore());
+                if(current.getScore() > max) {
+                    move = dir;
+                    max = current.getScore();
+                    score = current.getScore();
+                }
+            }
+        }
+        if(move == null || model.getScore() == score){
+            Random random = new Random();
+            move = dirs[random.nextInt(dirs.length)];
+        }
+        System.out.println(move);
+        return move;
     }
 
     @Override
@@ -22,6 +50,16 @@ public class GreedyAI implements AI {
     }
 
     public static void main(String[] args) {
+        TilesModel model = new TilesModel();
+        GreedyAI greedyAI = new GreedyAI();
         
+        while (!model.isGameOver()) {
+            Direction move = greedyAI.chooseMove(model);
+            model.move(move);
+        }
+        
+        greedyAI.addData(model);
+
+        System.out.println("Game Over! Final Score: " + model.getScore());
     }
 }
